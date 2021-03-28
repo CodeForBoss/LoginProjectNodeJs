504c745cb312a371df7a53e3c6aa937203426dbb
MotionBlue <ape.7107@outlook.com>
2021-03-28 12:24:39 +0600

    all yes

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ app.use(express.static('HtmlFile'));
 app.get('/',(req,res) => {
     res.sendFile(__dirname+"/HtmlFile/home.html");
 });
-app.get('/signup',(req,res) => {
-    
-});
 app.listen(4000,() => {
     console.log('listening on port 4000');
 });
